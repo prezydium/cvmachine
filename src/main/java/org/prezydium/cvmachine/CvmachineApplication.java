f6dd4cdb0869134f2fab1898895a2ee517c2dfc3
Krzysztof Syska <prezydium834@gmail.com>
2018-07-28 09:26:13 +0200

    Initial commit

---
+++ src/main/java/org/prezydium/cvmachine/CvmachineApplication.java
@@ -0,0 +1,12 @@
+package org.prezydium.cvmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvmachineApplication.class, args);
+	}
+}
