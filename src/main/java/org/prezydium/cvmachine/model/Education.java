1c54b1f021fe13e6b1171d85fba0c808b2cf5368
Krzysztof Syska <prezydium834@gmail.com>
2018-07-29 17:28:23 +0200

    Added education support

--- src/main/java/org/prezydium/cvmachine/model/Education.java
+++ src/main/java/org/prezydium/cvmachine/model/Education.java
@@ -1,12 +1,64 @@
 package org.prezydium.cvmachine.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
 public class Education {
 
-    private LocalDate beginningDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+
     private String schoolName;
+    
     private String description;
 
+    public Education() {
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Education{");
+        sb.append("startDate=").append(startDate);
+        sb.append(", endDate=").append(endDate);
+        sb.append(", schoolName='").append(schoolName).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
