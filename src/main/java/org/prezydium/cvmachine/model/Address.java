09d8805a0b4167e58889ca91e19012b5bb447d82
Krzysztof Syska <prezydium834@gmail.com>
2018-07-29 15:35:45 +0200

    Bound user data from form to model

--- src/main/java/org/prezydium/cvmachine/model/Address.java
+++ src/main/java/org/prezydium/cvmachine/model/Address.java
@@ -2,7 +2,44 @@ package org.prezydium.cvmachine.model;
 
 public class Address {
 
-    private String streetWithBuildingAndApartment;
+    private String street;
     private String postalCode;
     private String city;
+
+    public Address() {
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Address{");
+        sb.append("street='").append(street).append('\'');
+        sb.append(", postalCode='").append(postalCode).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- src/main/java/org/prezydium/cvmachine/model/UserData.java
+++ src/main/java/org/prezydium/cvmachine/model/UserData.java
@@ -5,8 +5,71 @@ public class UserData {
     private String firstName;
     private String lastName;
     private Address address;
-    private String telephoneNumber;
+    private String phone;
     private String linkedInProfile;
-    private String scvLink;
+    private String vcsRepo;
 
+    public UserData() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLinkedInProfile() {
+        return linkedInProfile;
+    }
+
+    public void setLinkedInProfile(String linkedInProfile) {
+        this.linkedInProfile = linkedInProfile;
+    }
+
+    public String getVcsRepo() {
+        return vcsRepo;
+    }
+
+    public void setVcsRepo(String vcsRepo) {
+        this.vcsRepo = vcsRepo;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("UserData{");
+        sb.append("firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", address=").append(address);
+        sb.append(", phone='").append(phone).append('\'');
+        sb.append(", linkedInProfile='").append(linkedInProfile).append('\'');
+        sb.append(", vcsRepo='").append(vcsRepo).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
